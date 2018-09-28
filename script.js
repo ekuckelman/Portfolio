dc600dc7c314961fff53ecc77da8514d6bd5de1c
Emily Kuckelman <ekuckelman@gmail.com>
2018-09-28 16:48:57 -0600

    Ready for deployment

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-var aboutNavBottom = document.querySelector('.about-nav-bottom');
 var aboutNavTop = document.querySelector('.about-nav-top');
-var aboutTitle = document.querySelector('.about-title')
+var aboutNavBottom = document.querySelector('.about-nav-bottom');
+var aboutTitle = document.querySelector('.about-title');
+var projectsNavTop = document.querySelector('.projects-nav-top');
+var projectsNavBottom = document.querySelector('.projects-nav-bottom');
+
+var projectsTitle = document.querySelector('.projects-title');
 
 aboutNavBottom.addEventListener('click', aboutFocus);
 aboutNavTop.addEventListener('click', aboutFocus);
-
+projectsNavBottom.addEventListener('click', projectsFocus);
+projectsNavTop.addEventListener('click', projectsFocus);
 
 
 function aboutFocus() {
   aboutTitle.scrollIntoView();
+}
+
+function projectsFocus() {
+  projectsTitle.scrollIntoView();
 }
 
